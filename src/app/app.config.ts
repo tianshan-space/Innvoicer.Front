a2418ca13d16bceee695545d81de64e977fdbc4b
Giorgi <gigola9@yahoo.com>
2025-01-12 16:19:30 +0400

    layout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ConfigService } from './core/services/config.service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions()), // Check
+    provideHttpClient(
+    //  withInterceptors([responseInterceptor, authInterceptor]),
+    ),
+    provideAppInitializer(()=> {
+      const initFn = ((configService: ConfigService) => {
+        return () => configService.getConfig()
+      })(inject(ConfigService));
+      return initFn();
+    }),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { featureRouters } from './features/routes';
+import { LoginComponent } from './auth/container/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    canActivate: [authGuard],
+    loadComponent: () => import('./core/containers/main/main.component').then((m) => m.MainComponent),
+    children: [...featureRouters]
+  }
+];
