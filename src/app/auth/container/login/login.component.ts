dd2b68efa71836400d903d3fa33d507b5b60d521
Mikheil <84536014+mberrishdev@users.noreply.github.com>
2025-01-24 12:28:16 +0400

    add loader and favicon.ico

--- src/app/auth/container/login/login.component.ts
+++ src/app/auth/container/login/login.component.ts
@@ -1,44 +1,68 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { AuthService } from '../../services/auth.service';
-import { catchError } from 'rxjs';
+import { catchError, finalize } from 'rxjs';
 import { Router } from '@angular/router';
-import { AuthModel } from '../../models/AuthModel';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   standalone: true,
-  imports: [ButtonModule, FloatLabelModule, InputTextModule, ReactiveFormsModule],
+  imports: [
+    ButtonModule,
+    FloatLabelModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
   errorOccurred = false;
+  loading = false;
 
   private authService = inject(AuthService);
   private router = inject(Router);
 
   loginForm = new FormGroup({
     email: new FormControl({ value: '', disabled: false }, Validators.required),
-    password: new FormControl({ value: '', disabled: false }, Validators.required)
+    password: new FormControl(
+      { value: '', disabled: false },
+      Validators.required
+    ),
   });
 
   login() {
+    if (this.loginForm.invalid) return;
+
+    this.loading = true;
+    this.errorOccurred = false;
+
     const res = {
       email: this.loginForm.get('email')!.value as string,
-      password: this.loginForm.get('password')!.value as string
-    }
-    this.authService.login(res).pipe(
-      catchError((err) => {
-        this.errorOccurred = true;
-        return err;
-      })
-    ).subscribe((m) => {
-      localStorage.setItem('auth', JSON.stringify(m));
-      this.router.navigateByUrl('/invoices');
-    });
+      password: this.loginForm.get('password')!.value as string,
+    };
+
+    this.authService
+      .login(res)
+      .pipe(
+        catchError((err) => {
+          this.errorOccurred = true;
+          return err;
+        }),
+        finalize(() => (this.loading = false))
+      )
+      .subscribe((m) => {
+        localStorage.setItem('auth', JSON.stringify(m));
+        this.router.navigateByUrl('/invoices');
+      });
   }
 }
